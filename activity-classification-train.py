3a694a924b4b48d99368587de3de9b016e98f174
Thuccy Nguyen <48234257+thuccynguyen@users.noreply.github.com>
2021-04-29 13:12:54 -0400

    added activity classification starter code

---
+++ activity-classification-train.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+"""
+This is the script used to train an activity recognition 
+classifier on accelerometer data.
+
+"""
+
+import os
+import sys
+import numpy as np
+from sklearn import model_selection, metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
+from sklearn.tree import export_graphviz, DecisionTreeClassifier
+from features import extract_features
+from util import slidingWindow, reorient, reset_vars
+import pickle
+import scipy
+
+# %%---------------------------------------------------------------------------
+#           Load in Data
+# -----------------------------------------------------------------------------
+
+print("Loading data...")
+sys.stdout.flush()
+data_file = 'my-activity-data.csv'
+data = np.genfromtxt(data_file, delimiter=',')
+print("Loaded {} raw labelled activity data samples.".format(len(data)))
+sys.stdout.flush()
+
+#print(data[0])
+# %%---------------------------------------------------------------------------
+#
+#		                    Pre-processing
+
+#
+# -----------------------------------------------------------------------------
+
+print("Reorienting accelerometer data...")
+sys.stdout.flush()
+reset_vars()
+reoriented = np.asarray([reorient(data[i,4], data[i,3], data[i,2]) for i in range(len(data))])
+#print(reoriented[0])
+reoriented_data_with_timestamps = np.append(data[:,0:1],reoriented,axis=1)
+#print(reoriented_data_with_timestamps[0])
+data = np.append(reoriented_data_with_timestamps, data[:,-1:], axis=1)
+
+#print(data[0])
+# %%---------------------------------------------------------------------------
+#
+#		                Extract Features & Labels
+#
+# -----------------------------------------------------------------------------
+
+window_size = 20
+step_size = 20
+
+# sampling rate should be about 25 Hz; you can take a brief window to confirm this
+n_samples = 1000
+time_elapsed_seconds = (data[n_samples,0] - data[0,0]) / 1000
+sampling_rate = n_samples / time_elapsed_seconds
+
+# TODO: list the class labels that you collected data for in the order of label_index (defined while collecting data)
+class_names = ["sitting", "walking", "standing", "jogging"] #...
+
+print("Extracting features and labels for window size {} and step size {}...".format(window_size, step_size))
+sys.stdout.flush()
+
+X = []
+Y = []
+
+for i,window_with_timestamp_and_label in slidingWindow(data, window_size, step_size):
+    window = window_with_timestamp_and_label[:,1:-1] 
+    feature_names, x = extract_features(window)
+    X.append(x)
+    Y.append(window_with_timestamp_and_label[10, -1]) #label index
+
+    
+X = np.asarray(X) #feature vectors
+Y = np.asarray(Y)
+n_features = len(X)
+    
+print("Finished feature extraction over {} windows".format(len(X)))
+print("Unique labels found: {}".format(set(Y)))
+print("\n")
+sys.stdout.flush()
+
+
+# %%---------------------------------------------------------------------------
+#
+#		                Train & Evaluate Classifier
+#
+# -----------------------------------------------------------------------------
+
+
+# TODO: split data into train and test datasets using 10-fold cross validation
+
+cv = model_selection.KFold(n_splits=10, random_state=None, shuffle=True)
+
+"""
+TODO: iterating over each fold, fit a decision tree classifier on the training set.
+Then predict the class labels for the test set and compute the confusion matrix
+using predicted labels and ground truth values. Print the accuracy, precision and recall
+for each fold.
+"""
+precisions = []
+recalls = []
+tree = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+for train_index, test_index in cv.split(X):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    
+    tree = tree.fit(X_train,Y[train_index])
+    y_pred = tree.predict(X_test) #goal is y_pred = Y[test_index]
+    conf = confusion_matrix(Y[test_index], y_pred) #?? 
+    print(conf)
+
+    for i in range(len(conf)):
+        true_pos = conf[i][i]
+        total = 0
+        for j in range(len(conf)):
+            total += conf[j][i]
+        precision = true_pos/total
+        precisions.append(precision)
+
+    for i in range(len(conf)):
+        true_pos = conf[i][i]
+        total = 0
+        for j in range(len(conf)):
+            total += conf[i][j]
+        recall = true_pos/total
+        recalls.append(recall)
+
+
+# TODO: calculate and print the average accuracy, precision and recall values over all 10 folds
+
+print("average precision:", np.mean(precisions))
+print("average recall:", np.mean(recalls))
+
+array = []
+# TODO: train the decision tree classifier on entire dataset
+
+tree = tree.fit(X,Y)
+
+
+# TODO: Save the decision tree visualization to disk - replace 'tree' with your decision tree and run the below line
+
+export_graphviz(tree, out_file='tree.dot', feature_names = feature_names)
+
+# TODO: Save the classifier to disk - replace 'tree' with your decision tree and run the below line
+with open('classifier.pickle', 'wb') as f:
+    pickle.dump(tree, f)
+
---
+++ features.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+This file is used for extracting features over windows of tri-axial accelerometer 
+data. We recommend using helper functions like _compute_mean_features(window) to 
+extract individual features.
+
+As a side note, the underscore at the beginning of a function is a Python 
+convention indicating that the function has private access (although in reality 
+it is still publicly accessible).
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+import math 
+
+def _compute_mean_features(window):
+    return np.mean(window, axis=0)
+
+# TODO: define functions to compute more features
+def _compute_standard_deviation(window):
+    return np.std(window,axis = 0)
+
+def compute_magnitude(window):
+    magnitude = []
+    for i in range(len(window)):
+        magnitude.append(math.sqrt(window[i,0]**2+ window[i,1]**2+ window[i,2]**2))
+    return np.array(magnitude)
+
+def _compute_magnitude_peaks(window):
+    magnitude = compute_magnitude(window)
+    peaks, _ = find_peaks(magnitude)
+    return np.array([len(peaks)])
+
+
+#Compute FFT features - Dominant Frequency over Magnitude
+def _compute_magnitude_dominant_frequency(window):
+    magnitude = compute_magnitude(window)
+    return [max(np.fft.rfft(magnitude).astype(float))]
+
+#Compute FFT features - Dominant Frequency over X Axis
+def _compute_x_dominant_frequency(window):
+    return np.fft.rfft(window[:0]).astype(float)
+
+#Compute FFT features - Dominant Frequency over Y Axis
+def _compute_y_dominant_frequency(window):
+    return np.fft.rfft(window[:1]).astype(float)
+
+#Compute FFT features - Dominant Frequency over Z Axis
+def _compute_z_dominant_frequency(window):
+    return np.fft.rfft(window[:2]).astype(float)
+
+#Compute entropy
+def _entropy(window):
+    hist, bin_edges = np.histogram(window, density=True) #change bin?
+    hist = hist/hist.sum() 
+    sum = 0
+    for i in hist:
+        if i!= 0:
+            sum += i * math.log(i)
+    return [sum] #needs to be 1D
+    
+   
+
+
+def extract_features(window):
+    """
+    Here is where you will extract your features from the data over 
+    the given window. We have given you an example of computing 
+    the mean and appending it to the feature vector.
+    
+    """
+    
+    x = []
+    feature_names = []
+
+    x.append(_compute_mean_features(window))
+    x.append(_compute_standard_deviation(window))
+    x.append(_compute_magnitude_peaks(window))
+    x.append(_compute_magnitude_dominant_frequency(window))
+    x.append(_entropy(window))
+    feature_names.append("x_mean")
+    feature_names.append("y_mean")
+    feature_names.append("z_mean")
+
+    # TODO: call functions to compute other features. Append the features to x and the names of these features to feature_names
+    feature_names.append("x_std")
+    feature_names.append("y_std")
+    feature_names.append("z_std")
+    feature_names.append("magnitude_peaks")
+    feature_names.append("magnitude_dominant_frequency")
+    feature_names.append("entropy")
+    
+    feature_vector = np.concatenate(x, axis=0) # convert the list of features to a single 1-dimensional vector
+    return feature_names, feature_vector
