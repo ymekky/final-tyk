247f9363868d7a1a050dc9aa4ef9e404bf8030ed
YASMEEN <yasmeenmekky@gmail.com>
2021-05-05 02:31:34 +0200

    Clean up code

--- activity-classification-train.py
+++ activity-classification-train.py
@@ -28,7 +28,6 @@ data = np.genfromtxt(data_file, delimiter=',')
 print("Loaded {} raw labelled activity data samples.".format(len(data)))
 sys.stdout.flush()
 
-#print(data[0])
 # %%---------------------------------------------------------------------------
 #
 #		                    Pre-processing
@@ -39,13 +38,7 @@ sys.stdout.flush()
 print("Reorienting accelerometer data...")
 sys.stdout.flush()
 reset_vars()
-#reoriented = np.asarray([reorient(data[i,4], data[i,3], data[i,2]) for i in range(len(data))])
-#print(reoriented[0])
-#reoriented_data_with_timestamps = np.append(data[:,0:1],data,axis=1)
-#print(reoriented_data_with_timestamps[0])
-#data = np.append(reoriented_data_with_timestamps, data[:,-1:], axis=1)
 
-#print(data[0])
 # %%---------------------------------------------------------------------------
 #
 #		                Extract Features & Labels
@@ -55,13 +48,11 @@ reset_vars()
 window_size = 20
 step_size = 5
 
-# sampling rate should be about 25 Hz; you can take a brief window to confirm this
 n_samples = len(data)-1
 time_elapsed_seconds = (data[n_samples,0] - data[0,0]) / 1000
 sampling_rate = n_samples / time_elapsed_seconds
 
-# TODO: list the class labels that you collected data for in the order of label_index (defined while collecting data)
-class_names = ["crunches", "planks", "bicycle crunches", "mountain climbers"] #...
+class_names = ["crunches", "planks", "bicycle crunches", "mountain climbers"]
 
 print("Extracting features and labels for window size {} and step size {}...".format(window_size, step_size))
 sys.stdout.flush()
@@ -71,7 +62,6 @@ Y = []
 
 for i,window_with_timestamp_and_label in slidingWindow(data, window_size, step_size):
     window = window_with_timestamp_and_label[:,2:-1] 
-    #print(window)
     feature_names, x = extract_features(window)
     X.append(x)
     Y.append(window_with_timestamp_and_label[10, -1]) #label index
@@ -86,36 +76,24 @@ print("Unique labels found: {}".format(set(Y)))
 print("\n")
 sys.stdout.flush()
 
-
 # %%---------------------------------------------------------------------------
 #
 #		                Train & Evaluate Classifier
 #
 # -----------------------------------------------------------------------------
 
-
-# TODO: split data into train and test datasets using 10-fold cross validation
-
 cv = model_selection.KFold(n_splits=10, random_state=None, shuffle=True)
 
-"""
-TODO: iterating over each fold, fit a decision tree classifier on the training set.
-Then predict the class labels for the test set and compute the confusion matrix
-using predicted labels and ground truth values. Print the accuracy, precision and recall
-for each fold.
-"""
 precisions = []
 recalls = []
 accuracies = []
 tree = DecisionTreeClassifier(criterion="entropy", max_depth=3)
 for train_index, test_index in cv.split(X):
-    #print("TRAIN:", train_index, "TEST:", test_index)
     X_train, X_test = X[train_index], X[test_index]
     
     tree = tree.fit(X_train,Y[train_index])
-    y_pred = tree.predict(X_test) #goal is y_pred = Y[test_index]
+    y_pred = tree.predict(X_test)
     conf = confusion_matrix(Y[test_index], y_pred) 
-    #print(conf)
 
     for i in range(len(conf)):
         true_pos = conf[i][i]
@@ -134,23 +112,14 @@ for train_index, test_index in cv.split(X):
         recalls.append(recall)
     accuracies.append(accuracy_score(Y[test_index],y_pred))
 
-
-# TODO: calculate and print the average accuracy, precision and recall values over all 10 folds
 print("average accuracy:", np.mean(accuracies))
 print("average precision:", np.mean(precisions))
 print("average recall:", np.mean(recalls))
 
-
-# TODO: train the decision tree classifier on entire dataset
-
 tree = tree.fit(X,Y)
-
-
-# TODO: Save the decision tree visualization to disk - replace 'tree' with your decision tree and run the below line
 
 export_graphviz(tree, out_file='tree.dot', feature_names = feature_names, class_names = class_names)
 
-# TODO: Save the classifier to disk - replace 'tree' with your decision tree and run the below line
 with open('classifier.pickle', 'wb') as f:
     pickle.dump(tree, f)
 
